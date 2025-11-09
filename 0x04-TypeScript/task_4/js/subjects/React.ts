47e5fcf947c0f7f861fb59fabca7e12168650ad3
Amoako Domson Wahab <143537125+amoakodomson25@users.noreply.github.com>
2025-11-09 02:29:20 +0000

    editings

--- 0x04-TypeScript/task_4/js/subjects/React.ts
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -5,18 +5,14 @@ namespace Subjects {
 
   export class React extends Subject {
     getRequirements(): string {
-      return "Here is the list of requirements for React";
+      return 'Here is the list of requirements for React';
     }
 
     getAvailableTeacher(): string {
-      if (
-        this.teacher &&
-        this.teacher.experienceTeachingC &&
-        this.teacher.experienceTeachingC > 0
-      ) {
-        return `Available Teacher: ${this.teacher.firstName}`;
+      if (this.teacher && this.teacher.experienceTeachingReact && this.teacher.experienceTeachingReact > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`; 
       }
-      return "No available teacher";
+      return 'No available teacher';
     }
   }
 }
