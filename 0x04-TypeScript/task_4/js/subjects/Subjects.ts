d74cc6467d802b7ddab5224b3f397e51cf90269e
Amoako Domson Wahab <143537125+amoakodomson25@users.noreply.github.com>
2025-11-09 02:14:27 +0000

    edits

--- 0x04-TypeScript/task_4/js/subjects/Subjects.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subjects.ts
@@ -1,6 +1,6 @@
 namespace Subjects {
     export class Subject {
-      private teacher: Teacher | null = null;
+      public teacher: Teacher | null = null;
   
       public setTeacher(teacher: Teacher): void {
         this.teacher = teacher;
