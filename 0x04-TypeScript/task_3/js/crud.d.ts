b3fa6b371476d3476318bd6f9dac444ae3919183
Amoako Domson Wahab <143537125+amoakodomson25@users.noreply.github.com>
2025-11-09 01:53:28 +0000

    Update crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -2,7 +2,7 @@
 import { RowID, RowElement } from './interface';
 
 // Declared the CRUD functions with type signatures
-declare function insertRow(row: RowElement): RowID;
+declare function insertRow(row: RowElement): number;
 declare function updateRow(rowId: RowID, row: RowElement): void;
 declare function deleteRow(rowId: RowID): void;
 declare function getRow(rowId: RowID): RowElement;
